915a4b23f2ef8e620dab1465d2461b52fcc11b36
Mario Cezzare Angelicola Chiodi <mcezzare@gmail.com>
2016-12-01 20:38:24 -0200

    update start.sh
    remove existing pid, just fix an issue

--- docker-build/start.sh
+++ docker-build/start.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+set -e
 
-source /etc/apache2/envvars
-exec apache2 -D FOREGROUND
+: "${APACHE_CONFDIR:=/etc/apache2}"
+: "${APACHE_ENVVARS:=$APACHE_CONFDIR/envvars}"
+if test -f "$APACHE_ENVVARS"; then
+	. "$APACHE_ENVVARS"
+fi
+
+# Apache gets grumpy about PID files pre-existing
+: "${APACHE_PID_FILE:=${APACHE_RUN_DIR:=/var/run/apache2}/apache2.pid}"
+rm -f "$APACHE_PID_FILE"
+
+exec apache2 -DFOREGROUND "$@"
